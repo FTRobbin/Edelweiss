00b5f1990c3dbfef149e7b934249356db9e82ead
SongChujun <a925504340@gmail.com>
2018-08-11 04:03:09 +0800

    AdversartStat modified

--- src/Stat/AdversaryStat.py
+++ src/Stat/AdversaryStat.py
@@ -10,18 +10,20 @@ from Protocols.HerdingWithBroadcast import *
 from Adversaries.SynHerdingBenchmarkAttacker import *
 def SynHerdingValidityAttackStat():
     input=50*[1]+50*[0]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    times=100
     k=4
     _lambda_loacl=1
     setting = SynchronousByzantine(100, input, PossibleAdversaries[4],
-                                   PossibleControllers[0], f=0, tf=0, protocol=Herding,
+                                   PossibleControllers[0], f=0, tf=0, protocol=HerdingWithBroadcast,
                                    measure=[ByzValidity,
                                             ByzConsistency, ByzUnanimity],
                                    centralized=True, centralized_adversary=PossibleAdversaries[5], seed=None, _lambda=_lambda_loacl,k=1)
-    f_list=[27,33,38,44,50,55]
+    f_list=[4,7,10,13,16,19,22]
     setting.set_k(k)
+    print("times="+str(times))
     print("k="+str(k))
     print("lambda="+str(_lambda_loacl))
     for f in f_list:
         print("f="+str(f))
         tf=f
-        stat(setting,input,f,tf,_lambda_loacl,10000)
+        stat(setting,input,f,tf,_lambda_loacl,times)
