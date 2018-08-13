fea7e672a3aae879dbd9be09e8ff97dcb128def4
SongChujun <a925504340@gmail.com>
2018-08-14 07:55:27 +0800

    Stat moduled modified

--- src/PushButtonStat/HerdingStating.py
+++ src/PushButtonStat/HerdingStating.py
@@ -22,6 +22,16 @@ def HerdingStating(h, input, times, _lambda_list, k_list, protocol_list, adversa
                         SetStatParaAndRun(
                             h, setting, input, times, f, tf, protocol, adversary, _lambda, k)
                         print(" ", file=h)
+def CustomizedHerdingStating(h,para_setting_list):
+    setting = SynchronousByzantine(None, None, None,
+                                   PossibleControllers[0], f=None, tf=None, protocol=None,
+                                   measure=[ByzValidity,
+                                            ByzConsistency, ByzUnanimity],
+                                   centralized=None, centralized_adversary=None, seed=None, _lambda=None, k=None)
+    for para_setting in para_setting_list:
+        SetStatParaAndRun(
+                            h, setting, para_setting[0], para_setting[1], para_setting[2], para_setting[3], para_setting[4], para_setting[5], para_setting[6], para_setting[7])
+        print(" ", file=h)
 
 def HerdingStatingAll(times=1):
     h = open("HerdingStating.txt", "w+")
@@ -41,9 +51,15 @@ def HerdingStatingAll(times=1):
     f_list = [20, 25, 30, 35, 40, 45, 50, 55, 60]
     HerdingStating(h, input, times, _lambda_list, k_list, protocol_list, adversary_list, f_list)
     # Experiment 4: Lambda
-    _lambda_list = [3, 4, 5]
-    adversary_list = [PossibleAdversaries[7]]
-    k_list = [10]
-    HerdingStating(h, input, times, _lambda_list, k_list, protocol_list, adversary_list, f_list)
+    _lambda_list=[3,4,5]
+    para_setting_list=[]
+    for _lambda in _lambda_list:
+        for f in f_list:
+            k=int(100/(_lambda*_lambda))
+            tf=f
+            # h, setting, input, times, f, tf, protocol, adversary, _lambda, k)
+            para_setting=(input,times,f,tf,HerdingWithBroadcastFast,PossibleAdversaries[7],_lambda,k)
+            para_setting_list.append(para_setting)
+    CustomizedHerdingStating(h,para_setting_list)
     h.close()
 
